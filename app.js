1969d5f3b7aac66693c24d76e020a69732404353
maateh <mateharak01@gmail.com>
2023-10-17 18:49:24 +0200

    added eslint

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 const express = require('express')
 
+const app = express()
+
 // middlewares
-app.use(bodyParser.json())
+app.use(express.json())
 
 // routes
 // const exampleRoute = require('./routes/exampleRoute')
 // app.use('/exampleRoute', exampleRoute)
-
-const app = express()
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
