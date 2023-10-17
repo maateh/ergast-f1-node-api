bca8bb325ad5e052fba3ddc8bb68fa6ae15a560f
maateh <mateharak01@gmail.com>
2023-10-17 18:38:47 +0200

    added dependencies and gitignore

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // Ergast F1 Database NodeJS API
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+
+const express = require('express')
+
+// middlewares
+app.use(bodyParser.json())
+
+// routes
+// const exampleRoute = require('./routes/exampleRoute')
+// app.use('/exampleRoute', exampleRoute)
+
+const app = express()
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Server listening on PORT ${port}`)
+})
