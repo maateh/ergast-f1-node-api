66a41a58ca533f281233d0f96858f4b36aa31527
maateh <mateharak01@gmail.com>
2023-10-21 12:17:40 +0200

    modified drivers controller

--- src/api/controllers/drivers.js
+++ src/api/controllers/drivers.js
@@ -1,16 +1,52 @@
 const Driver = require('../models/driver')
+const Season = require('../models/season')
+const Race = require('../models/race')
 
 module.exports = {
   getAllDrivers: (req, res, next) => {
-    res.status(200).json({})
+    Driver.findAll()
+      .then(drivers => {
+        res.status(200).json(drivers)
+      })
+      .catch(err => {
+        // TODO: error handling
+        res.status(400).json({ success: false })
+        console.log('getAllDrivers: ', err)
+      })
   },
   getDriverInformation: (req, res, next) => {
-    res.status(200).json({})
+    Driver.findOne({ where: { driverRef: req.params.id }})
+      .then(driver => {
+        res.status(200).json(driver)
+      })
+      .catch(err => {
+        // TODO: error handling
+        res.status(400).json({ success: false })
+        console.log('getDriverInformation: ', err)
+      })
   },
   getDriversWithinAYear: (req, res, next) => {
-    res.status(200).json({})
+    // TODO: create associations
+    // Season.findOne({ where: { year: req.params.year }})
+    //   .then(drivers => {
+    //     res.status(200).json(drivers)
+    //   })
+    //   .catch(err => {
+    //     // TODO: error handling
+    //     console.log('getDriversWithinAYear: ', err)
+    //   })
+    res.status(200).json({ success: true })
   },
   getDriversWithinARace: (req, res, next) => {
-    res.status(200).json({})
+    // TODO: create associations
+    // Race.findOne({ where: { year: req.params.year, round: req.params.round }})
+    //   .then(drivers => {
+    //     res.status(200).json(drivers)
+    //   })
+    //   .catch(err => {
+    //     // TODO: error handling
+    //     console.log('getDriversWithinARace: ', err)
+    //   })
+    res.status(200).json({ success: true })
   }
 }
