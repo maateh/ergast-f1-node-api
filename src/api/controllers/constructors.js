7a071c998957b7e763ee479ff0f4e5aac45cfd5a
maateh <mateharak01@gmail.com>
2023-10-21 12:14:55 +0200

    modified constructors controller

--- src/api/controllers/constructors.js
+++ src/api/controllers/constructors.js
@@ -1,16 +1,52 @@
 const Constructor = require('../models/constructor')
+const Season = require('../models/season')
+const Race = require('../models/race')
 
 module.exports = {
   getAllConstructors: (req, res, next) => {
-    res.status(200).json({})
+    Constructor.findAll()
+      .then(constructors => {
+        res.status(200).json(constructors)
+      })
+      .catch(err => {
+        // TODO: error handling
+        res.status(400).json({ success: false })
+        console.log('getAllConstructors: ', err)
+      })
   },
   getConstructorInformation: (req, res, next) => {
-    res.status(200).json({})
+    Constructor.findOne({ where: { constructorRef: req.params.id }})
+      .then(constructor => {
+        res.status(200).json(constructor)
+      })
+      .catch(err => {
+        // TODO: error handling
+        res.status(400).json({ success: false })
+        console.log('getConstructorInformation: ', err)
+      })
   },
   getConstructorsWithinAYear: (req, res, next) => {
-    res.status(200).json({})
+    // TODO: create associations
+    // Season.findOne({ where: { year: req.params.year }})
+    //   .then(constructors => {
+    //     res.status(200).json(constructors)
+    //   })
+    //   .catch(err => {
+    //     // TODO: error handling
+    //     console.log('getConstructorsWithinAYear: ', err)
+    //   })
+    res.status(200).json({ success: true })
   },
   getConstructorsWithinARace: (req, res, next) => {
-    res.status(200).json({})
+    // TODO: create associations
+    // Race.findOne({ where: { year: req.params.year, round: req.params.round }})
+    //   .then(constructors => {
+    //     res.status(200).json(constructors)
+    //   })
+    //   .catch(err => {
+    //     // TODO: error handling
+    //     console.log('getConstructorsWithinARace: ', err)
+    //   })
+    res.status(200).json({ success: true })
   }
 }
