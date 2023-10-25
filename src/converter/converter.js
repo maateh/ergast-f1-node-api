f768b0fd6daeb7de60658d5efeee3c8937b895e0
maateh <mateharak01@gmail.com>
2023-10-25 10:16:42 +0200

    constructor converter

--- src/converter/converter.js
+++ src/converter/converter.js
@@ -7,13 +7,15 @@ const circuitConverter = require('./converters/circuit')
 const seasonConverter = require('./converters/season')
 const weekendConverter = require('./converters/weekend')
 const driverConverter = require('./converters/driver')
+const constructorConverter = require('./converters/constructor')
 
 const startConversion = async () => {
   try {
     // await circuitConverter()
     // await seasonConverter()
     // await weekendConverter()
-    await driverConverter()
+    // await driverConverter()
+    await constructorConverter()
   } catch (err) {
     console.error('An error occured during conversion: ', err)
   }
---
+++ src/converter/converters/constructor.js
@@ -0,0 +1,37 @@
+const db = require('../database/mysql')
+
+// models
+const Constructor = require('../../api/models/constructor')
+
+const getAllConstructors = () => {
+  const query = 'SELECT * FROM constructors'
+
+  return db.execute(query)
+    .then(([constructors]) => constructors)
+    .catch(err => {
+      console.error('Query error: ', err)
+    })
+}
+
+const conversion = () => {
+  return getAllConstructors()
+    .then(constructors => {
+      const convertedConstructors = constructors.map(constructor => {
+        return new Constructor({
+          ergastId: constructor.constructorId,
+          ref: constructor.constructorRef,
+          name: constructor.name,
+          nationality: constructor.nationality,
+          wiki: constructor.url
+        })
+      })
+      // console.log('convertedConstructors: ', convertedConstructors)
+
+      return Constructor.insertMany(convertedConstructors)
+    })
+    .catch(err => {
+      console.error('Conversion error: ', err)
+    })
+}
+
+module.exports = conversion
