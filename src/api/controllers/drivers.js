45795ed22da510854b8b68604fa17bf7419126c2
maateh <mateharak01@gmail.com>
2023-10-28 09:44:52 +0200

    drivers route & controller

--- src/api/controllers/drivers.js
+++ src/api/controllers/drivers.js
@@ -1,60 +1,62 @@
-const getAllDrivers = (req, res, next) => {
-  res.status(200).json({ success: true })
+// models
+const Driver = require('../models/driver')
+const Season = require('../models/season')
+const Weekend = require('../models/weekend')
 
-  // Driver.findAll()
-  //   .then(drivers => {
-  //     res.status(200).json(drivers)
-  //   })
-  //   .catch(err => {
-  //     // TODO: error handling
-  //     res.status(400).json({ success: false })
-  //     console.log('getAllDrivers: ', err)
-  //   })
+const getAllDrivers = (req, res, next) => {
+  Driver.find()
+    .then(drivers => {
+      res.status(200).json(drivers)
+    })
+    .catch(err => {
+      // TODO: error handling
+      res.status(400).json({ success: false })
+      console.log('getAllDrivers: ', err)
+    })
 }
 
 const getDriverInformation = (req, res, next) => {
   const { id } = req.params
-  res.status(200).json({ success: true })
 
-  // Driver.findOne({ where: { driverRef: id }})
-  //   .then(driver => {
-  //     res.status(200).json(driver)
-  //   })
-  //   .catch(err => {
-  //     // TODO: error handling
-  //     res.status(400).json({ success: false })
-  //     console.log('getDriverInformation: ', err)
-  //   })
+  Driver.findOne({ ref: id })
+    .then(driver => {
+      res.status(200).json(driver)
+    })
+    .catch(err => {
+      // TODO: error handling
+      res.status(400).json({ success: false })
+      console.log('getDriverInformation: ', err)
+    })
 }
 
 const getDriversWithinASeason = (req, res, next) => {
   const { year } = req.params
-  res.status(200).json({ success: true })
 
-  // TODO: create associations
-  // Season.findOne({ where: { year }})
-  //   .then(drivers => {
-  //     res.status(200).json(drivers)
-  //   })
-  //   .catch(err => {
-  //     // TODO: error handling
-  //     console.log('getDriversWithinASeason: ', err)
-  //   })
+  // TODO: save drivers (+teams)
+  // separately on the Season model
+  Season.findOne({ year }).populate('drivers._driver')
+    .then(season => {
+      const drivers = season.drivers.map(d => d._driver)
+      res.status(200).json(drivers)
+    })
+    .catch(err => {
+      // TODO: error handling
+      console.log('getDriversWithinASeason: ', err)
+    })
 }
 
 const getDriversWithinAWeekend = (req, res, next) => {
   const { year, round } = req.params
-  res.status(200).json({ success: true })
 
-  // TODO: create associations
-  // Weekend.findOne({ where: { year, round }})
-  //   .then(drivers => {
-  //     res.status(200).json(drivers)
-  //   })
-  //   .catch(err => {
-  //     // TODO: error handling
-  //     console.log('getDriversWithinAWeekend: ', err)
-  //   })
+  Weekend.findOne({ year, round }).populate('drivers._driver')
+    .then(weekend => {
+      const drivers = weekend.drivers.map(d => d._driver)
+      res.status(200).json(drivers)
+    })
+    .catch(err => {
+      // TODO: error handling
+      console.log('getDriversWithinAWeekend: ', err)
+    })
 }
 
 module.exports = {
--- src/api/routes/drivers.js
+++ src/api/routes/drivers.js
@@ -7,13 +7,13 @@ const router = express.Router()
 // List of all drivers
 router.get('/', controller.getAllDrivers)
 
-// Driver information
-router.get('/:id', controller.getDriverInformation)
-
 // List of all drivers within a season
 router.get('/year/:year', controller.getDriversWithinASeason)
 
 // List of all drivers within a weekend in a season
 router.get('/year/:year/round/:round', controller.getDriversWithinAWeekend)
+
+// Driver information
+router.get('/:id', controller.getDriverInformation)
 
 module.exports = router
