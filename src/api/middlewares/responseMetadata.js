f053ac8e95e37e68ac85f52e0b1150218e0a5ba3
maateh <mateharak01@gmail.com>
2023-11-04 09:53:09 +0100

    route & query are now separated in metadata

--- src/api/middlewares/responseMetadata.js
+++ src/api/middlewares/responseMetadata.js
@@ -1,7 +1,8 @@
 const responseMetadata = (req, res, next) => {
   res.locals.metadata = {
     url: req.protocol + '://' + req.headers.host + req.originalUrl,
-    route: req.originalUrl
+    route: req.path,
+    query: Object.keys(req.query).length ? req.query : undefined
   }
   next()
 }
