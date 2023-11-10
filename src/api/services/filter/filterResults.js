cd4b46e3c54a68a69a0daf238a39436a8df2a38f
maateh <mateharak01@gmail.com>
2023-11-10 14:32:13 +0100

    a little performance 'boost' to filterResults service

--- src/api/services/filter/filterResults.js
+++ src/api/services/filter/filterResults.js
@@ -20,75 +20,89 @@ const filterResults = async (
     sprint: 1
   }
 ) => {
-  const total = await Result.aggregate([
+  const [{ total, results }] = await Result.aggregate([
     { $match: filter },
-    { $count: 'total' }
-  ])
-
-  const results = await Result.aggregate([
-    { $match: filter },
-    { $sort: sort },
-    { $skip: pagination.offset },
-    { $limit: pagination.limit },
     {
-      $lookup: {
-        from: 'weekends',
-        foreignField: '_id',
-        localField: 'weekend._weekend',
-        as: 'weekend'
+      $facet: {
+        totalCount: [{
+          $group: {
+            _id: null,
+            total: { $sum: 1 }
+          }
+        }],
+        paginatedData: [
+          { $sort: sort },
+          { $skip: pagination.offset },
+          { $limit: pagination.limit },
+          {
+            $lookup: {
+              from: 'weekends',
+              foreignField: '_id',
+              localField: 'weekend._weekend',
+              as: 'weekend'
+            }
+          },
+          { $unwind: '$weekend' },
+          {
+            $lookup: {
+              from: 'seasons',
+              foreignField: '_id',
+              localField: 'weekend.season._season',
+              as: 'weekend.season'
+            }
+          },
+          { $unwind: '$weekend.season' },
+          {
+            $lookup: {
+              from: 'circuits',
+              foreignField: '_id',
+              localField: 'weekend.circuit._circuit',
+              as: 'weekend.circuit'
+            }
+          },
+          { $unwind: '$weekend.circuit' },
+          {
+            $lookup: {
+              from: 'drivers',
+              foreignField: '_id',
+              localField: 'driver._driver',
+              as: 'driver'
+            }
+          },
+          { $unwind: '$driver' },
+          {
+            $lookup: {
+              from: 'teams',
+              foreignField: '_id',
+              localField: 'team._team',
+              as: 'team'
+            }
+          },
+          { $unwind: '$team' },
+          {
+            $project: {
+              _id: 0,
+              driver: 1,
+              team: 1,
+              weekend: 1,
+              ...requiredResults,
+            }
+          }
+        ]
       }
     },
-    { $unwind: '$weekend' },
-    {
-      $lookup: {
-        from: 'seasons',
-        foreignField: '_id',
-        localField: 'weekend.season._season',
-        as: 'weekend.season'
-      }
-    },
-    { $unwind: '$weekend.season' },
-    {
-      $lookup: {
-        from: 'circuits',
-        foreignField: '_id',
-        localField: 'weekend.circuit._circuit',
-        as: 'weekend.circuit'
-      }
-    },
-    { $unwind: '$weekend.circuit' },
-    {
-      $lookup: {
-        from: 'drivers',
-        foreignField: '_id',
-        localField: 'driver._driver',
-        as: 'driver'
-      }
-    },
-    { $unwind: '$driver' },
-    {
-      $lookup: {
-        from: 'teams',
-        foreignField: '_id',
-        localField: 'team._team',
-        as: 'team'
-      }
-    },
-    { $unwind: '$team' },
+    { $unwind: '$totalCount' },
     {
       $project: {
-        _id: 0,
-        driver: 1,
-        team: 1,
-        weekend: 1,
-        ...requiredResults,
+        total: '$totalCount.total',
+        results: '$paginatedData'
       }
     }
   ])
 
   return {
-    results,
-    total: total.length ? total[0].total : 0
+    total,
+    results
   }
 }
 
