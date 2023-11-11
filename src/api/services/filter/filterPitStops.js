1fbaeaaec52deedd735ed28b09e8f2e97ea1a90d
maateh <mateharak01@gmail.com>
2023-11-11 10:07:42 +0100

    created: filterPitStops service

---
+++ src/api/services/filter/filterPitStops.js
@@ -0,0 +1,97 @@
+// models
+const Weekend = require('../../models/Weekend')
+const PitStop = require('../../models/PitStop')
+
+const filterPitStops = async (
+  filter = {},
+  pagination = { limit: 30, offset: 0 },
+  sort = {}
+) => {
+  const [weekend] = await Weekend.aggregate([
+    {
+      $match: {
+        'season.year': filter['season.year'],
+        round: filter['weekend.round'],
+      }
+    },
+    {
+      $lookup: {
+        from: 'seasons',
+        foreignField: '_id',
+        localField: 'season._season',
+        as: 'season'
+      }
+    },
+    { $unwind: '$season' },
+    {
+      $lookup: {
+        from: 'circuits',
+        foreignField: '_id',
+        localField: 'circuit._circuit',
+        as: 'circuit'
+      }
+    },
+    { $unwind: '$circuit' },
+    { $project: { _id: 0, __v: 0, ergastId: 0 } }
+  ])
+
+  const [{ pitStops, total }] = await PitStop.aggregate([
+    { $match: filter },
+    {
+      $facet: {
+        totalCount: [{
+          $group: {
+            _id: null,
+            total: { $sum: 1 }
+          }
+        }],
+        paginatedData: [
+          { $sort: sort },
+          { $skip: pagination.offset },
+          { $limit: pagination.limit },
+          {
+            $lookup: {
+              from: 'drivers',
+              foreignField: '_id',
+              localField: 'driver._driver',
+              as: 'driver'
+            }
+          },
+          { $unwind: '$driver' },
+          {
+            $lookup: {
+              from: 'teams',
+              foreignField: '_id',
+              localField: 'team._team',
+              as: 'team'
+            }
+          },
+          { $unwind: '$team' },
+          {
+            $project: {
+              _id: 0,
+              __v: 0,
+              season: 0,
+              weekend: 0
+            }
+          }
+        ]
+      }
+    },
+    { $unwind: '$totalCount' },
+    {
+      $project: {
+        pitStops: '$paginatedData',
+        total: '$totalCount.total'
+      }
+    }
+  ])
+
+  return {
+    weekend,
+    pitStops,
+    total
+  }
+}
+
+module.exports = filterPitStops
