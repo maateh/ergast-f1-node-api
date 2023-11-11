cb1059b8f444daa62e1aef9ed42d464508b1ca7f
maateh <mateharak01@gmail.com>
2023-11-11 09:54:45 +0100

    pitStops route constants added to the config

--- src/api/config/constants.js
+++ src/api/config/constants.js
@@ -3,11 +3,12 @@ module.exports = {
     circuits: '(/circuits/:circuitId)?',
     drivers: '(/drivers/:driverId)?',
     teams: '(/teams/:teamId)?',
-    timings: '(/lap/:timingLapNumber)?(/position/:timingPosition)?(/duration/:timingDuration)?',
     results: {
       race: '(/race(/position/:racePosition)?(/grid/:raceGrid)?(/fastest/:raceFastest)?(/points/:racePoints)?)?',
       qualifying: '(/qualifying(/position/:qualifyingPosition)?)?',
       sprint: '(/sprint(/position/:sprintPosition)?(/grid/:sprintGrid)?(/points/:sprintPoints)?)?'
-    }
+    },
+    timings: '(/lap/:timingLapNumber)?(/position/:timingPosition)?(/duration/:timingDuration)?',
+    pitStops: '(/stop/:pitStop)?(/lap/:pitStopLap)?(/duration/:pitStopDuration)?'
   }
 }
