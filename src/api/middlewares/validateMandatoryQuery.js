907f4f5171446805509868704c45b1daac9ca54e
maateh <mateharak01@gmail.com>
2023-11-23 18:24:27 +0100

    documentation comment is added to validateMandatoryQuery middleware

--- src/api/middlewares/validateMandatoryQuery.js
+++ src/api/middlewares/validateMandatoryQuery.js
@@ -1,9 +1,20 @@
 // errors
 const MissingMandatoryQueryError = require('../errors/MissingMandatoryQueryError')
 
+/**
+ * Middleware to validate mandatory query parameters based on the request URL.
+ * Throws a MissingMandatoryQueryError if parameters are missing.
+ *
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @param {Function} next - Express next function to pass control to the next middleware.
+ * @throws {MissingMandatoryQueryError} - If mandatory query parameters are missing.
+ */
+
 const validateMandatoryQuery = (req, res, next) => {
   const { originalUrl, query } = req
 
+  // Define mandatory query parameters based on the request URL
   const mandatoryFields = []
   if (originalUrl.includes('/timings') || originalUrl.includes('/pitstops')) {
     mandatoryFields.push('year')
@@ -14,9 +25,11 @@ const validateMandatoryQuery = (req, res, next) => {
     mandatoryFields.push('year')
   }
 
+  // Check for missing mandatory fields in the query parameters
   const queryKeys = Object.keys(query)
   const missing = mandatoryFields.some(field => !queryKeys.includes(field))
 
+  // If mandatory fields are missing, throw an error
   if (missing) {
     const error = new MissingMandatoryQueryError(mandatoryFields)
     next(error)
