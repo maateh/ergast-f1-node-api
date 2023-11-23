fd34b202445941034c21d9673a4a53bc534c58ed
maateh <mateharak01@gmail.com>
2023-11-23 18:21:14 +0100

    'year' is added as mandatory query param to standings without a filter #18

--- src/api/routes/standingsRoutes.js
+++ src/api/routes/standingsRoutes.js
@@ -5,6 +5,7 @@ const { getDriverStandings } = require('../controllers/standings/driverStandings
 const { getTeamStandings } = require('../controllers/standings/teamStandingsController')
 
 // middlewares
+const validateMandatoryQuery = require('../middlewares/validateMandatoryQuery')
 const responsePagination = require('../middlewares/responsePagination')
 const filterParser = require('../middlewares/filterParser')
 
@@ -13,11 +14,25 @@ const { ROUTE_FILTERS: { standings } } = require('../config/constants')
 
 const router = express.Router()
 
+// Get driver standings of a specific year
+router.get(
+  '/driver',
+  [validateMandatoryQuery, responsePagination, filterParser],
+  getDriverStandings
+)
+
 // Get driver standings which match the specified filter
 router.get(
   `/driver(/:driverId)?${standings}`,
   [responsePagination, filterParser],
   getDriverStandings
+)
+
+// Get team standings of a specific year
+router.get(
+  '/team',
+  [validateMandatoryQuery, responsePagination, filterParser],
+  getTeamStandings
 )
 
 // Get team standings which match the specified filter
